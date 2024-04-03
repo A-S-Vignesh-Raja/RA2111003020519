a6c5deb6cd0403a6df836d8b230d48a49572c12d
A-S-Vignesh-Raja <a.s.vigneshraja@gmail.com>
2024-04-03 20:49:23 +0530

    added reg process

---
+++ afford-intern/src/api/reg/response.js
@@ -0,0 +1,20 @@
+reg="http://20.244.56.144/test/register"
+
+response={
+	"companyName":"goMart",
+    "ownerName":"Vignesh Raja",
+    "rollNo":"RA2111003020519",
+    "ownerEmail":"ar6256@srmist.edu.in",
+    "accessCode":"bntKpm"}
+
+auth="http://20.244.56.144/test/auth"  
+
+response={
+        "companyName": "goMart",
+        "clientID": "e5f9dd85-10e9-4d54-9971-26417a394641",
+        "clientSecret": "ETYqOMSlvFSNlEGv",
+        "ownerName": "Vignesh Raja",
+        "ownerEmail": "ar6256@srmist.edu.in",
+        "rollNo": "RA2111003020519"
+}
+
