5d17441966997a63edc2701cc84c011f8fbc0b67
A-S-Vignesh-Raja <a.s.vigneshraja@gmail.com>
2024-04-03 19:42:51 +0530

    test

--- afford-intern/src/api/index.js
+++ afford-intern/src/api/index.js
@@ -3,7 +3,7 @@ const bodyParser=require("body-parser");
 const app=express();
 app.use(bodyParser.urlencoded({extended:true}));
 app.get("/",function(req,res){
-    res.sendFile(__dirname+"/index.html");
+    res.sendFile(__dirname+"/calculator");
 });
 app.post("/",function(req,res){
     var num1=Number(req.body.num1);
