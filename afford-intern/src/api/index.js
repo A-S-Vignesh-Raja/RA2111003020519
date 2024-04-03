6dbc647d16ddc77ebd325f5e4773632d9049fecf
A-S-Vignesh-Raja <a.s.vigneshraja@gmail.com>
2024-04-03 20:54:35 +0530

    edited

--- afford-intern/src/api/index.js
+++ afford-intern/src/api/index.js
@@ -8,8 +8,8 @@ app.get("/",function(req,res){
 app.post("/",function(req,res){
     var num1=Number(req.body.num1);
     var num2=Number(req.body.num2);
-    var result=num1+num2;
-    res.send("The Sum of two numbers is "+result);
+    var result=(num1+num2)/2;
+    res.send("The Average of two numbers is "+result);
 });
 
 app.listen(3000,function(){
